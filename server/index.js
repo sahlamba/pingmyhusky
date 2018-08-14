ae7a38f7857277a81b7405da28d01cb845597277
thedrumsknight <sahil.lamba95@gmail.com>
2018-08-14 18:29:05 +0530

    Use express router

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const routes = require('./routes');
+
+app.use(routes);
 
 app.listen(3000, err => {
   if (err) throw err;
