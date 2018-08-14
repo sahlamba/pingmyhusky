f82e2e72dab2705083bb6018273fa9b11482e476
thedrumsknight <sahil.lamba95@gmail.com>
2018-08-14 18:34:50 +0530

    Serve index.html

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
 const routes = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World!').status(200);
+  res.render('index.html');
 });
 
 module.exports = router;
