343d58f22f0817fe0cf8fb9991d7d85bcd2f3da9
thedrumsknight <sahil.lamba95@gmail.com>
2018-08-15 21:29:08 +0530

    Setup sound control route, client request

--- server/public/main.js
+++ server/public/main.js
@@ -1,4 +1,6 @@
-const socket = io.connect('http://localhost:9042');
+const BASE_URL = 'http://localhost:9042';
+
+const socket = io.connect(BASE_URL);
 
 socket.on('server:data', function(data) {
   console.log(data);
@@ -9,6 +11,26 @@ socket.on('server:data', function(data) {
 socket.on('relay:stream_data', function(stream_data) {
   console.log(stream_data);
 });
+
+function playSound() {
+  let req = new XMLHttpRequest();
+
+  req.open('GET', BASE_URL + '/control/sound?play=sound.mp3', true);
+
+  req.onreadystatechange = function() {
+    if (req.readyState === 4) {
+      const responseDiv = document.getElementById('play-sound-response');
+      if (req.status === 200) {
+        responseDiv.innerHTML = req.response;
+      }
+      if (req.status === 400) {
+        responseDiv.innerHTML = req.statusText;
+      }
+    }
+  };
+
+  req.send(null);
+}
 
 // Use this to pipe to JSMPEG Player
 // https://github.com/ShinobiCCTV/jsmpeg-pipe
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,15 @@ router.get('/', (req, res) => {
   res.render('index.html');
 });
 
+router.get('/control/sound', (req, res) => {
+  if (req && req.query && req.query.play && req.query.play.length) {
+    const soundFile = req.query.play;
+    console.log('[control] Sound requested: ', soundFile);
+    res.json({ success: true, operation: 'control-sound', query: req.query });
+  }
+  res.statusCode = 400;
+  res.statusMessage = 'Invalid query in req: ' + JSON.stringify(req.query);
+  res.end();
+});
+
 module.exports = router;
